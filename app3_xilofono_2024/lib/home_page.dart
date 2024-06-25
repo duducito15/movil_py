d036cb1f421fba19a75ed35a3df7281db3b3904a
duducito15 <echavezv@utea.edu.pe>
2024-06-25 07:14:52 -0500

    dependencia instalada

--- app3_xilofono_2024/lib/home_page.dart
+++ app3_xilofono_2024/lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:audioplayers/audioplayers.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
