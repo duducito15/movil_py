ee4905cb5e042da3e86c7204f241a86d232fbd17
duducito15 <echavezv@utea.edu.pe>
2024-06-25 07:44:42 -0500

    Appbar

--- app3_xilofono_2024/lib/home_page.dart
+++ app3_xilofono_2024/lib/home_page.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatelessWidget {
+  AudioCache audioCache = AudioCache();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Xylophone App",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("Click!!!"),
+        ),
+      ),
+    );
   }
 }
