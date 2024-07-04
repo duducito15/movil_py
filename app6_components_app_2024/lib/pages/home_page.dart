9e0a5dff62e2314408212794783e50e208d92abe
duducito15 <84535246+duducito15@users.noreply.github.com>
2024-07-04 07:57:46 -0500

    App6 - ListTile

--- app6_components_app_2024/lib/pages/home_page.dart
+++ app6_components_app_2024/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,7 +8,80 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Text("Hola!!!"),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 200.0,
+              width: 200.0,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(20.0),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/img/david_chicle.png"),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.75),
+                    offset: Offset(5, 5),
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Flutter Components",
+              style: GoogleFonts.poppins(
+                fontSize: 23.0,
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(
+              width: 185.0,
+              child: Divider(),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 10.0,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 12.0,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.black45,
+                ),
+                title: Text(
+                  "Avatar",
+                  style: GoogleFonts.poppins(),
+                ),
+                subtitle: Text(
+                  "Ir a detalle de Avatar",
+                  style: GoogleFonts.poppins(),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.black45,
+                ),
+              ),
+            ),
           ],
         ),
       ),
