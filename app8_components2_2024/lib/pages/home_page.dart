a7466f2d8ec620ae1651b8b19f9ba9d616bcc2a2
Dudu <echavezv@utea.edu.pe>
2024-08-07 07:29:59 -0500

    Avatar circular y textos

--- app8_components2_2024/lib/pages/home_page.dart
+++ app8_components2_2024/lib/pages/home_page.dart
@@ -6,26 +6,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffedf2f4),
+      backgroundColor: const Color(0xffedf2f4),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
               Container(
-                padding: EdgeInsets.all(14.0),
+                padding: const EdgeInsets.all(14.0),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(16.0),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.black.withOpacity(0.25),
-                      offset: Offset(0, 7),
+                      offset: const Offset(0, 7),
                       blurRadius: 16.0,
                     ),
                   ],
                 ),
-                child: Column(
+                child: const Column(
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -41,6 +41,35 @@ class HomePage extends StatelessWidget {
                           size: 18.0,
                         ),
                       ],
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                      ),
+                      backgroundColor: Colors.amber,
+                      radius: 48.0,
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Text(
+                      "Hira Riaz",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Color(0xff1b263b),
+                      ),
+                    ),
+                    Text(
+                      "UX/UI Designer",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0,
+                        color: Colors.black87,
+                      ),
                     ),
                   ],
                 ),
