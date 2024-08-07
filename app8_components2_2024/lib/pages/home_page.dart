dcb12fc39d019ffb00cd0223ffea795964315f36
Dudu <echavezv@utea.edu.pe>
2024-08-07 07:18:48 -0500

    Iconos container

--- app8_components2_2024/lib/pages/home_page.dart
+++ app8_components2_2024/lib/pages/home_page.dart
@@ -6,7 +6,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hola Mundo"),
+      backgroundColor: Color(0xffedf2f4),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(14.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25),
+                      offset: Offset(0, 7),
+                      blurRadius: 16.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.align_horizontal_left,
+                          color: Color(0xff1b263b),
+                          size: 18.0,
+                        ),
+                        Icon(
+                          Icons.more_vert,
+                          color: Color(0xff1b263b),
+                          size: 18.0,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
