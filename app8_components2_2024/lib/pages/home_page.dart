22cf7da882b5f27ef9e4b12738d337044e0f53d2
Dudu <echavezv@utea.edu.pe>
2024-08-08 06:53:35 -0500

    Diseño terminado

--- app8_components2_2024/lib/pages/home_page.dart
+++ app8_components2_2024/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app8_components2_2024/widgets/item_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,134 +9,179 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffedf2f4),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(14.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.25),
-                      offset: const Offset(0, 7),
-                      blurRadius: 16.0,
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(14.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        offset: const Offset(0, 7),
+                        blurRadius: 16.0,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.align_horizontal_left,
+                            color: Color(0xff1b263b),
+                            size: 18.0,
+                          ),
+                          Icon(
+                            Icons.more_vert,
+                            color: Color(0xff1b263b),
+                            size: 18.0,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                        ),
+                        backgroundColor: Colors.amber,
+                        radius: 48.0,
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      Text(
+                        "Hira Riaz",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color(0xff1b263b),
+                        ),
+                      ),
+                      Text(
+                        "UX/UI Designer",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.0,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "\$8900",
+                                style: TextStyle(
+                                  color: Color(0xff1b263b),
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("Income"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 40,
+                            width: 30,
+                            child: VerticalDivider(
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "\$8900",
+                                style: TextStyle(
+                                  color: Color(0xff1b263b),
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("Income"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 40,
+                            width: 30,
+                            child: VerticalDivider(
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "\$8900",
+                                style: TextStyle(
+                                  color: Color(0xff1b263b),
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("Income"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                child: Column(
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(
-                          Icons.align_horizontal_left,
-                          color: Color(0xff1b263b),
-                          size: 18.0,
+                        Text(
+                          "Overview",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22.0,
+                            color: Color(0xff1b263b),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 6.0,
                         ),
                         Icon(
-                          Icons.more_vert,
+                          Icons.notification_add,
+                          size: 20.0,
                           color: Color(0xff1b263b),
-                          size: 18.0,
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                      ),
-                      backgroundColor: Colors.amber,
-                      radius: 48.0,
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
                     Text(
-                      "Hira Riaz",
+                      "Sept 13, 2020",
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Color(0xff1b263b),
-                      ),
-                    ),
-                    Text(
-                      "UX/UI Designer",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.0,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "\$8900",
-                              style: TextStyle(
-                                color: Color(0xff1b263b),
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text("Income"),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 40,
-                          width: 30,
-                          child: VerticalDivider(
-                            color: Colors.black.withOpacity(0.3),
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              "\$8900",
-                              style: TextStyle(
-                                color: Color(0xff1b263b),
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text("Income"),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 40,
-                          width: 30,
-                          child: VerticalDivider(
-                            color: Colors.black.withOpacity(0.3),
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              "\$8900",
-                              style: TextStyle(
-                                color: Color(0xff1b263b),
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text("Income"),
-                          ],
-                        ),
-                      ],
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14.0,
+                          color: Color(0xff1b263b)),
                     ),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 22.0,
+                ),
+                ItemListWidget(),
+                ItemListWidget(),
+                ItemListWidget(),
+                ItemListWidget(),
+                ItemListWidget(),
+              ],
+            ),
           ),
         ),
       ),
