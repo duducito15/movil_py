dee4565949d6bc548f4c3724499de3fbdce0428b
Dudu <echavezv@utea.edu.pe>
2024-08-20 07:09:52 -0500

    Drawer Header

--- app9_shared_2024/lib/pages/home_page.dart
+++ app9_shared_2024/lib/pages/home_page.dart
@@ -11,6 +11,57 @@ class HomePage extends StatelessWidget {
           "Shared Preferences App",
         ),
       ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg"),
+                ),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg"),
+                    ),
+                    Text(
+                      "Fiorella Gonzales Torres",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Administrador",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("Body"),
+        ],
+      ),
     );
   }
 }
