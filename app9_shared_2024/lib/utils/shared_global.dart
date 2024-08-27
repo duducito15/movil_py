f99b25406786f0ea3501d135029d4dab5d8a79d0
Dudu <echavezv@utea.edu.pe>
2024-08-27 07:17:38 -0500

    SharedGlobal

---
+++ app9_shared_2024/lib/utils/shared_global.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedGlobal {
+  static final SharedGlobal myInstance = SharedGlobal();
+
+  SharedGlobal._instance();
+
+  factory SharedGlobal() {
+    return myInstance;
+  }
+
+  late SharedPreferences _preferences;
+
+  Future<void> initSharedPreferences() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  set fullName(String value){
+    _preferences.setString("fullName", value);
+  }
+
+  String get fullName{
+    return _preferences.getString("fullName") ?? "-";
+  }
+}
