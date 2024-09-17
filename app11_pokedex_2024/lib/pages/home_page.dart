63e8f4bd34111a7c62920a03501ce4504d762306
Dudu <echavezv@utea.edu.pe>
2024-09-17 07:41:03 -0500

    Inicio PokemonModel

--- app11_pokedex_2024/lib/pages/home_page.dart
+++ app11_pokedex_2024/lib/pages/home_page.dart
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                         ),
                       )
                       .toList(),
-                )
+                ),
               ],
             ),
           ),
