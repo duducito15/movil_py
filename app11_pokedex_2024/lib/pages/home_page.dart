5dbeb585b9caca8369bf1f59f424dbc5ed93af56
Dudu <echavezv@utea.edu.pe>
2024-09-18 07:28:28 -0500

    detail page

--- app11_pokedex_2024/lib/pages/home_page.dart
+++ app11_pokedex_2024/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:app11_pokedex_2024/models/pokemon_model.dart';
 import 'package:app11_pokedex_2024/ui/widgets/item_pokemon_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -11,6 +12,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List pokemons = [];
+
+  List<PokemonModel> pokemonModel = [];
 
   @override
   void initState() {
@@ -24,7 +27,11 @@ class _HomePageState extends State<HomePage> {
     http.Response response = await http.get(_uri);
     if (response.statusCode == 200) {
       Map<String, dynamic> myMap = json.decode(response.body);
-      pokemons = myMap["pokemon"];
+      //pokemons = myMap["pokemon"];
+      pokemonModel = myMap["pokemon"]
+          .map<PokemonModel>((e) => PokemonModel.fromJson(e))
+          .toList();
+      //print(pokemonModel);
       setState(() {});
       //print(pokemons[0]["img"]);
       //pokemons.forEach((e) {
@@ -62,13 +69,12 @@ class _HomePageState extends State<HomePage> {
                   mainAxisSpacing: 12,
                   crossAxisSpacing: 12,
                   childAspectRatio: 1.3,
-                  children: pokemons
+                  children: pokemonModel
                       .map(
                         (e) => ItemPokemonWidget(
-                          name: e["name"],
-                          img: e["img"],
-                          type:
-                              List<String>.from(e["type"].map((item) => item)),
+                          name: e.name,
+                          img: e.img,
+                          type: e.type,
                         ),
                       )
                       .toList(),
--- app11_pokedex_2024/lib/ui/widgets/item_pokemon_widget.dart
+++ app11_pokedex_2024/lib/ui/widgets/item_pokemon_widget.dart
@@ -1,3 +1,4 @@
+import 'package:app11_pokedex_2024/pages/detail_page.dart';
 import 'package:app11_pokedex_2024/ui/widgets/item_type_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,64 +17,74 @@ class ItemPokemonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // print(type);
 
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.green.shade300,
-        borderRadius: BorderRadius.circular(18.0),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            child: Image.asset(
-              "assets/images/pokeball.png",
-              height: 120,
-              color: Colors.white.withOpacity(0.27),
-            ),
-            bottom: -20,
-            right: -20,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (contex) => DetailPage(),
           ),
-          Positioned(
-            child: Image.network(
-              img,
-              errorBuilder: (BuildContext context, Object exception,
-                  StackTrace? stackTrace) {
-                return Image.asset(
-                  "assets/images/whoisthat.png",
-                  height: 100,
-                );
-              },
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.green.shade300,
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              child: Image.asset(
+                "assets/images/pokeball.png",
+                height: 120,
+                color: Colors.white.withOpacity(0.27),
+              ),
+              bottom: -20,
+              right: -20,
             ),
-            bottom: -10,
-            right: -10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 15.0,
-              horizontal: 10.0,
+            Positioned(
+              child: Image.network(
+                img,
+                errorBuilder: (BuildContext context, Object exception,
+                    StackTrace? stackTrace) {
+                  return Image.asset(
+                    "assets/images/whoisthat.png",
+                    height: 100,
+                  );
+                },
+              ),
+              bottom: -10,
+              right: -10,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 10.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                ...type
-                    .map((e) => ItemTypeWidget(
-                          text: e,
-                        ))
-                    .toList(),
-                // Column(
-                //   children: type.map((e) => ItemTypeWidget()).toList(),
-                // ),
-              ],
+                  ...type
+                      .map((e) => ItemTypeWidget(
+                            text: e,
+                          ))
+                      .toList(),
+                  // Column(
+                  //   children: type.map((e) => ItemTypeWidget()).toList(),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
