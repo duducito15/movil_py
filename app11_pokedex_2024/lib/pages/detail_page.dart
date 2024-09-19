4eb39977ad9d93678b38d690d9eef1a912db3166
Dudu <echavezv@utea.edu.pe>
2024-09-19 06:49:19 -0500

    detail page v1

--- app11_pokedex_2024/lib/pages/detail_page.dart
+++ app11_pokedex_2024/lib/pages/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:app11_pokedex_2024/ui/widgets/item_type_widget.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -5,17 +6,126 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      backgroundColor: Colors.green.shade500,
+      backgroundColor: Colors.green.shade300,
       appBar: AppBar(
-        backgroundColor: Colors.green.shade500,
+        elevation: 0,
         foregroundColor: Colors.white,
+        backgroundColor: Colors.green.shade300,
         actions: [
           IconButton(
             onPressed: () {},
             icon: Icon(
               Icons.favorite_border,
+              color: Colors.white,
             ),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: height * 0.1,
+            right: -30,
+            child: Image.asset(
+              "assets/images/pokeball.png",
+              height: 200,
+              color: Colors.white.withOpacity(0.30),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Bulbasaur",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 6.0,
+                    ),
+                    Row(
+                      children: [
+                        ItemTypeWidget(text: "Grass"),
+                        ItemTypeWidget(text: "Poison"),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  "#001",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              const Expanded(
+                flex: 4,
+                child: SizedBox(),
+              ),
+              Expanded(
+                flex: 8,
+                child: SizedBox(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned.fill(
+                          top: -90,
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: Container(
+                              child: Image.network(
+                                "http://www.serebii.net/pokemongo/pokemon/001.png",
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "About Pokemon",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("Peso: "),
+                              Text("0.71 k"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
