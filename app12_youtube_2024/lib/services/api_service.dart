331d7376b13d90cd4e1ebdd75bb74be26b34a172
Dudu <echavezv@utea.edu.pe>
2024-10-03 07:16:47 -0500

    video_model completo, getVideos consumiento

--- app12_youtube_2024/lib/services/api_service.dart
+++ app12_youtube_2024/lib/services/api_service.dart
@@ -1,14 +1,23 @@
+import 'dart:convert';
+
+import 'package:app12_youtube_2024/models/video_model.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
-  getVideos() async {
+  Future<List<VideoModel>> getVideos() async {
+    List<VideoModel> videosModel = [];
     String _path =
         "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=AIzaSyAd0wmg95jGu3tspHf-f_lTbVW3spWpINU&regionCode=PE";
     Uri _uri = Uri.parse(_path);
-
     http.Response response = await http.get(_uri);
 
-    print(response.statusCode);
-    print(response.body);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> myMap = json.decode(response.body);
+      List videos = myMap["items"];
+      videosModel = videos.map((e) => VideoModel.fromJson(e)).toList();
+      //print(videosModel);
+      return videosModel;
+    }
+    return videosModel;
   }
 }
--- app12_youtube_2024/lib/ui/general/colors.dart
+++ app12_youtube_2024/lib/ui/general/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color kBrandPrimaryColor = Color(0xFF212121);
 const Color kBrandSecundaryColor = Color(0xFF383838);
+const Color kBrandTertiryColor = Colors.white;
